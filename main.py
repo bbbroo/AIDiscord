7f7a74bd99cb7076f39654a4230590cb0a4dac67
brookinsbarry <72997929+brookinsbarry@users.noreply.github.com>
2023-05-13 22:24:34 -0500

    Update persona change and reset

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-modelname = 'gpt-3.5'
+modelname = 'gpt-3.5-turbo'
 gpt35names=["!gpt-3.5-turbo","!gpt-3.5","!GPT-3.5","!GPT-3.5-turbo", "!gpt3.5"]
 gpt4names=["!gpt-4","!GPT-4", "!gpt4"]
 
@@ -49,6 +49,7 @@ To reset the AI's persona, type: !resetpersona```""")
             return
         if message.content == "!clear":
             messages = [{'role': "system", 'content': persona}]
+            return
         if any(message.content == c for c in gpt4names): 
             #messages = [{'role': "system", 'content': persona}]
             modelname = "gpt-4"
@@ -61,11 +62,11 @@ To reset the AI's persona, type: !resetpersona```""")
             return
         if message.content.startswith('!persona'):
             updated_persona = message.content.split('!persona')[1]
-            messages = [{'role': "system", 'content': updated_persona}]
+            messages.append({'role': "system", 'content': updated_persona})
             await message.channel.send("Updated persona to: " + updated_persona)
             return
         if message.content.startswith('!resetpersona'):
-            messages = [{'role': "system", 'content': persona}]
+            messages.append({'role': "system", 'content': persona})
             await message.channel.send("Persona has been reset!")
             return
     f = open("logs.txt", "a+")
